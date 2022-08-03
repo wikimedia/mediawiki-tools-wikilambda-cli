50cd4f83189769d3c159ae98998d153ed8053842
AAssaf <aassaf-ctr@wikimedia.org>
2022-08-03 15:52:13 +0200

    Fix indentation in formatting of lambda CLI output.
    The indentation was wrongly passed as the `lang` argument (presumably the call sites were not updated when the argument was added), so the effective indentation level was always 0.
    
    In this change I define a distinct `formatAtLevel()` function to make it clearer.
    
    Bug: T308771
    Change-Id: I8c2a01c5e418acfe91c9ec59900ed96dcedbbdd3

--- src/format.js
+++ src/format.js
@@ -6,102 +6,105 @@ const utils = require('./utils.js');
 
 // TODO: break into formatArray, formatString, formatType, etc.
 // TODO: do that before writing tests!
-const format = async (output, lang, indent = 0) => {
-  if (utils.isArray(output)) {
-    if (output.length === 0) {
-      return '[]';
-    }
-    let result = '[';
-    for (const element of output) {
-      result += '\n' + '  '.repeat(indent + 1);
-      result += await format(element, indent + 1);
-    }
-    result += '\n' + '  '.repeat(indent) + ']';
-    return result;
-  }
-  if (utils.isString(output)) {
-    if (utils.isZid(output)) {
-      return await labelize.labelizeId(output);
-    }
-    return '"' + output + '"';
-  }
-  if (utils.isObject(output)) {
-    if (output[c.ObjectType] === c.String) {
-      return '"' + output[c.StringValue] + '"';
-    }
-    if (output[c.ObjectType] === c.Type) {
-      let result = await labelize.labelizeId(c.Type) + ': ';
-      result += await format(output[c.TypeIdentity], indent + 1);
-      for (const key of output[c.TypeKeys]) {
+async function format(output, lang) {
+  async function formatAtLevel(output, indent = 0) {
+    if (utils.isArray(output)) {
+      if (output.length === 0) {
+        return '[]';
+      }
+      let result = '[';
+      for (const element of output) {
         result += '\n' + '  '.repeat(indent + 1);
-        result += await format(key[c.KeyType]) + ': ';
-        result += utils.getLabel(key[c.KeyLabels], lang);
+        result += await formatAtLevel(element, indent + 1);
       }
+      result += '\n' + '  '.repeat(indent) + ']';
       return result;
     }
-    if (output[c.ObjectType] === c.Function) {
-      let result = await format(output[c.FunctionIdentity], indent + 1);
-      result += ': ';
-      let first = true;
-      for (const key of output[c.FunctionArguments]) {
-        if (!first) {
-          result += ', ';
+    if (utils.isString(output)) {
+      if (utils.isZid(output)) {
+        return await labelize.labelizeId(output);
+      }
+      return '"' + output + '"';
+    }
+    if (utils.isObject(output)) {
+      if (output[c.ObjectType] === c.String) {
+        return '"' + output[c.StringValue] + '"';
+      }
+      if (output[c.ObjectType] === c.Type) {
+        let result = await labelize.labelizeId(c.Type) + ': ';
+        result += await formatAtLevel(output[c.TypeIdentity], indent + 1);
+        for (const key of output[c.TypeKeys]) {
+          result += '\n' + '  '.repeat(indent + 1);
+          result += await formatAtLevel(key[c.KeyType]) + ': ';
+          result += utils.getLabel(key[c.KeyLabels], lang);
         }
-        first = false;
-        result += await format(key[c.ArgumentType]);
+        return result;
       }
-      result += ' → ';
-      result += await format(output[c.FunctionReturntype]);
-      return result;
-    }
-    if (output[c.ObjectType] === c.Functioncall) {
-      let result = '';
-      let first = true;
+      if (output[c.ObjectType] === c.Function) {
+        let result = await formatAtLevel(output[c.FunctionIdentity], indent + 1);
+        result += ': ';
+        let first = true;
+        for (const key of output[c.FunctionArguments]) {
+          if (!first) {
+            result += ', ';
+          }
+          first = false;
+          result += await formatAtLevel(key[c.ArgumentType]);
+        }
+        result += ' → ';
+        result += await formatAtLevel(output[c.FunctionReturntype]);
+        return result;
+      }
+      if (output[c.ObjectType] === c.Functioncall) {
+        let result = '';
+        let first = true;
+        for (const key in output) {
+          if (key === c.ObjectType) {
+            continue;
+          }
+          if (key === c.FunctioncallFunction) {
+            result += await formatAtLevel(output[c.FunctioncallFunction]) + '(';
+            continue;
+          }
+          if (!first) {
+            result += ', ';
+          }
+          first = false;
+          result += await formatAtLevel(output[key]);
+        }
+        result += ')';
+        return result;
+      }
+      if (output[c.ObjectType] === c.Reference) {
+        return await formatAtLevel(output[c.ReferenceValue], indent);
+      }
+      if (output[c.ObjectType] === c.Boolean) {
+        return await formatAtLevel(output[c.BooleanValue], indent);
+      }
+      // if (Object.keys(output).length === 2) {
+      //  return await formatAtLevel(output[Object.keys(output)[1]], indent);
+      // }
+      let typeid = '';
+      if (utils.isZid(output[c.ObjectType])) {
+        typeid = output[c.ObjectType];
+      } else {
+        typeid = output[c.ObjectType][c.TypeIdentity];
+      }
+      let result = await labelize.labelizeId(typeid);
       for (const key in output) {
         if (key === c.ObjectType) {
           continue;
         }
-        if (key === c.FunctioncallFunction) {
-          result += await format(output[c.FunctioncallFunction]) + '(';
-          continue;
-        }
-        if (!first) {
-          result += ', ';
-        }
-        first = false;
-        result += await format(output[key]);
+        result += '\n' + '  '.repeat(indent + 1);
+        result += await labelize.labelizeId(key) + ': ';
+        result += await formatAtLevel(output[key], indent + 1);
       }
-      result += ')';
       return result;
     }
-    if (output[c.ObjectType] === c.Reference) {
-      return await format(output[c.ReferenceValue], indent);
-    }
-    if (output[c.ObjectType] === c.Boolean) {
-      return await format(output[c.BooleanValue], indent);
-    }
-    // if (Object.keys(output).length === 2) {
-    //  return await format(output[Object.keys(output)[1]], indent);
-    // }
-    let typeid = '';
-    if (utils.isZid(output[c.ObjectType])) {
-      typeid = output[c.ObjectType];
-    } else {
-      typeid = output[c.ObjectType][c.TypeIdentity];
-    }
-    let result = await labelize.labelizeId(typeid);
-    for (const key in output) {
-      if (key === c.ObjectType) {
-        continue;
-      }
-      result += '\n' + '  '.repeat(indent + 1);
-      result += await labelize.labelizeId(key) + ': ';
-      result += await format(output[key], indent + 1);
-    }
-    return result;
+    return output;
   }
-  return output;
-};
+  return formatAtLevel(output);
+}
 
 const formatTokens = (tokens, lang) => {
   let result = '';
